85ae5de4b1b6047114d0ccb3698399793a36f35f
Viktor <vvgrygorchuk@gmail.com>
2019-10-25 21:25:52 +0300

    Custom errors.

---
+++ se_wrapper/errors.py
@@ -0,0 +1,16 @@
+from re import compile
+
+from selenium.common.exceptions import WebDriverException
+
+
+
+
+
+
+class ElementNotClickableError(WebDriverException):
+    """ Special exception for cases where element can't receive a click. """
+
+    @staticmethod
+    def can_handle_exception(error: WebDriverException):
+        pattern = compile(r"is not clickable at point \([0-9]+, [0-9]+\). Other element would receive the click:")
+        return pattern.search(error.msg) is not None
