3a5acd44661aadf917db3f963da38c270b0f4978
Viktor <vvgrygorchuk@gmail.com>
2019-10-25 21:56:10 +0300

    Wrapp web element.

--- se_wrapper/element/web_element_wrapper.py
+++ se_wrapper/element/web_element_wrapper.py
@@ -5,6 +5,4 @@ class WebElementWrapper(ABC):
     """Abstract class for wrapped web element.
     Used for type reference. Shall be implemented in separate class.
     """
-
-    def __init__(self):
-        raise NotImplementedError()
+    raise NotImplementedError()
--- se_wrapper/element/wrapped_webelement.py
+++ se_wrapper/element/wrapped_webelement.py
@@ -1,32 +1,50 @@
+import time
+
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
+from selenium.webdriver.remote.webelement import WebElement
+
+from se_wrapper import help_utils
+from se_wrapper.browser_driver import BrowserDriver
 from se_wrapper.element.web_element_wrapper import WebElementWrapper
+from se_wrapper.errors import ElementNotClickableError
+
+
+DEFAULT_TIMEOUT = help_utils.DEFAULT_TIMEOUT
+TimeoutType = help_utils.TimeoutType
 
 
 class WrappedWebElement(WebElementWrapper):
-    """ Class is used to provide a lazy initialization of the WebElement.
+    """Class is used to provide a lazy initialization of the WebElement.
     WebElement is going to be searched only when is called.
-    So, web element can be declared in __init__ of the page class and be found only when needed for interaction.
+    Web element can be declared in __init__ of the page class and be found only when needed for interaction.
 
     """
 
-    def __init__(self, webdriver: DriverWrapper, selector, timeout=DEFAULT_TIMEOUT):
+    def __init__(self, webdriver: BrowserDriver, selector: str, timeout: TimeoutType = DEFAULT_TIMEOUT):
         self.webdriver = webdriver
         self.selector = selector
         self.timeout = timeout
+        self._element = None
 
     @property
     def web_element(self):
-        try:
-            return self.webdriver.find_element_by_selector(self.selector)
-        except NoSuchElementException as exc:
-            raise NoSuchElementException(f"Web Element with selector {self.selector} has not been found."
-                                         f"\n{exc.msg}")
+        """Get reference to Selenium webelement. """
+        if self._element is None:
+            try:
+                self._element = self.webdriver.find_element_by_selector(self.selector)
+            except NoSuchElementException as exc:
+                raise NoSuchElementException(f"Web Element with selector {self.selector} has not been found."
+                                             f"\n{exc.msg}")
+        return self._element
+
+    @web_element.setter
+    def web_element(self, element: WebElement):
+        self._element = element
 
     def __getattr__(self, attr):
-        """ Calls method or properties on self.web_element.
+        """Calls method or properties on self.web_element.
         Returns callable or attribute of WebElement.
         :param attr: any attr of the WebElement
-        :return: Any
-
         """
         try:
             orig_attr = self.web_element.__getattribute__(attr)
@@ -38,101 +56,17 @@ class WrappedWebElement(WebElementWrapper):
         except AttributeError as exc:
             raise AttributeError(f"WebElement has no attribute {attr}.\n{exc}")
 
-    def should_be_displayed(self, timeout=None):
-        """ Check for element to be visible on the html page.
-        :param timeout:
-        :return: boolean
-        """
-        timeout_ = timeout if timeout else self.timeout
-        return self.webdriver.wait_element_to_appear(self.selector, timeout=timeout_)
-
-    def should_have_class(self, expected_class: str, timeout=None):
-        """ Check for element to have a specific class.
-        :param expected_class: class_name for expected class (not css_selector).
-        :param timeout: how long to wait for condition.
-        :return: boolean
-        """
-        timeout_ = timeout if timeout else self.timeout
-        try:
-            return self.webdriver.wait_element_to_get_class(self.selector, expected_class, timeout=timeout_)
-        except TimeoutException:
-            return False
-
-    def should_have_element(self, child_selector, timeout=None):
-        """
-        Wait until web_element get a desired child element.
-        :param child_selector: str
-        :param timeout: int
-        :return: bool.
-        """
-        timeout_ = timeout if timeout else self.timeout
-        result = False
-        try:
-            if self.webdriver.wait_element_be_in_dom(child_selector, timeout=timeout_):
-                if self.find_element(by=self.webdriver.get_selector_type(child_selector), value=child_selector):
-                    result = True
-            return result
-        except TimeoutException:
-            raise TimeoutException(msg=f"{self.selector} has no element with locator='{child_selector}'")
-
-    def should_contain_text(self, text: str, timeout=None):
-        """ Checks element has provided text in texts attribute.
-        Not precise comparision, e.g. returns True for:
-        "some" in "this is some text", " test\n" and "test", "TEST" and "test"
-        :param text: str
-        :param timeout: int
-        :return: boolean
-        """
-        timeout_ = self.timeout
-        if timeout:
-            timeout_ = timeout
-        return self.webdriver.wait_element_have_similar_text(self.selector, text, timeout=timeout_)
-
-    def should_have_exact_text(self, text: str, timeout=None):
-        """ Checks element has exactly provided text, and no other text.
-        Precise comparision, e.g. returns False if "some" == "this is some text"
-        :param text: str
-        :param timeout: int
-        :return: boolean
-        """
-        timeout_ = self.timeout
-        if timeout:
-            timeout_ = timeout
-        if self.webdriver.wait_element_to_contain_text(self.selector, text, timeout=timeout_):
-            return self.webdriver.find_element_by_selector(self.selector).text == text
-        return False
-
-    def should_be_stale(self, timeout=None):
-        """ Check for web element to be stale or absent on the html page.
-        :param timeout: equal to the self.timeout if other not passed. :type: int
-        :return: boolean
-        """
-        timeout_ = self.timeout
-        if timeout:
-            timeout_ = timeout
-        return self.webdriver.wait_element_staleness(self.selector, timeout=timeout_)
-
-    def should_be_invisible(self, timeout=None):
-        """ Check for web element to be invisible to the end user.
-        :param timeout: equal to the self.timeout if other not passed. :type: int
-        :return: boolean
-        """
-        timeout_ = self.timeout
-        if timeout:
-            timeout_ = timeout
-        return self.webdriver.wait_element_invisibility(self.selector, timeout=timeout_)
-
-    def set_text_value(self, text):
-        """ Clears the input area before sending a new text key. """
+    def set_text_value(self, input_val):
+        """Clears the input area before sending a new text value."""
         self.web_element.clear()
-        self.web_element.send_keys(text)
+        self.web_element.send_keys(input_val)
 
     def double_click(self):
-        """ Don't want to do double click with ActionChains -) """
+        """Click web_element two times."""
         self.web_element.click()
         self.web_element.click()
 
-    def click(self, timeout=DEFAULT_TIMEOUT):
+    def click(self, timeout: TimeoutType = DEFAULT_TIMEOUT):
         """ Click element, throws ElementNotClickableException if element can't handle click.
         Wait with timeout, if click is not possible.
         """
@@ -151,5 +85,5 @@ class WrappedWebElement(WebElementWrapper):
                     raise WebDriverException(msg=exc.msg)
 
     def get_class(self):
-        """Get class of element """
+        """Get class of element."""
         return self.web_element.get_attribute("class")
