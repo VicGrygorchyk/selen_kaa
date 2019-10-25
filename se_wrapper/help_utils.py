e67ac7886b54ac402cbbe10fad7a9ec7a02b90d8
Viktor <vvgrygorchuk@gmail.com>
2019-10-25 21:24:42 +0300

    Added help methods.

---
+++ se_wrapper/help_utils.py
@@ -0,0 +1,26 @@
+from re import match
+from typing import Union
+
+from selenium.webdriver.common.by import By
+
+
+TimeoutType = Union[int, float]
+
+
+def get_selector_type(selector):
+    """ Checks if selector is css or xpath
+    >>>get_selector_type(".css")
+    css selector
+    >>>get_selector_type("#css")
+    css selector
+    >>>get_selector_type("some attr")
+    css selector
+    >>>get_selector_type("./dv//")
+    xpath
+    >>>get_selector_type("//div")
+    xpath
+    >>>get_selector_type("/div")
+    xpath
+    """
+    pattern_xpath = r"^(./)|^/"
+    return By.XPATH if match(pattern_xpath, selector) else By.CSS_SELECTOR
