865b437709446388bfdc522632b262abbe4031fd
Viktor <vvgrygorchuk@gmail.com>
2019-11-10 19:25:31 +0200

    Rename waits for element.

--- se_wrapper/element/element_waits.py
+++ se_wrapper/element/element_waits.py
@@ -12,7 +12,7 @@ class ElementWaits:
         self._web_element = web_element
         self._timeout = timeout
 
-    def to_be_visible(self, timeout: TimeoutType):
+    def be_visible(self, timeout: TimeoutType):
         """True when an element is visible on the html page.
         :param timeout: time to wait element visibility.
 
@@ -20,7 +20,7 @@ class ElementWaits:
         timeout_ = timeout if timeout else self._timeout
         return self._webdriver.wait_for.element_to_be_visible(self._web_element, timeout=timeout_)
 
-    def to_be_invisible(self, timeout: TimeoutType):
+    def be_invisible(self, timeout: TimeoutType):
         """True if an element is not visible on the html page.
         :param timeout: time to wait element visibility.
 
@@ -28,7 +28,7 @@ class ElementWaits:
         timeout_ = timeout if timeout else self._timeout
         return self._webdriver.wait_for.element_to_be_invisible(self._web_element, timeout_)
 
-    def to_have_class(self, expected_class: str, timeout: TimeoutType):
+    def have_class(self, expected_class: str, timeout: TimeoutType):
         """True when an element has a specific class.
         :param expected_class: class_name for expected class (not css_selector).
         :param timeout: time to wait for an element to have a class name.
@@ -37,7 +37,7 @@ class ElementWaits:
         timeout_ = timeout if timeout else self._timeout
         return self._webdriver.wait_for.element_to_get_class(self._web_element, expected_class, timeout_)
 
-    def to_include_element(self, child_css_selector: str, timeout: TimeoutType):
+    def include_element(self, child_css_selector: str, timeout: TimeoutType):
         """True when an element gets a desired child element.
         :param child_css_selector: a css selector for a child element.
         :param timeout: time to wait for the condition.
@@ -50,7 +50,7 @@ class ElementWaits:
             timeout_
         )
 
-    def to_contain_text(self, text: str, timeout: TimeoutType):
+    def contain_text(self, text: str, timeout: TimeoutType):
         """True if an element contains a provided text in its text attribute.
         :param text: expected text.
         :param timeout: time to wait for the condition.
@@ -61,7 +61,7 @@ class ElementWaits:
             timeout_ = timeout
         return self._webdriver.wait_for.element_to_contain_text(self._web_element, text, timeout_)
 
-    def to_have_similar_text(self, text: str, timeout: TimeoutType):
+    def have_similar_text(self, text: str, timeout: TimeoutType):
         """True if an element has a similar text in texts attribute.
         Not precise comparision, e.g. returns True for:
         "some" in "this is some text", " test\n" and "test", "TEST" and "test"
@@ -74,7 +74,7 @@ class ElementWaits:
             timeout_ = timeout
         return self._webdriver.wait_for.element_have_similar_text(self._web_element, text, timeout_)
 
-    def to_have_exact_text(self, text: str, timeout: TimeoutType):
+    def have_exact_text(self, text: str, timeout: TimeoutType):
         """True if an element has exactly provided text, and no other text.
         Precise comparision, e.g. returns False if "some" == "this is some text"
         :param text: exact text to search inside an element
@@ -97,7 +97,7 @@ class ElementWaits:
             timeout_ = timeout
         return self._webdriver.wait_for.no_element_in_dom(self._web_element, timeout_)
 
-    def to_be_on_the_screen(self, timeout: TimeoutType):
+    def be_on_the_screen(self, timeout: TimeoutType):
         """True for an element is present on the screen (inside the viewport).
         Checks if element's coordinates match viewport height and width.
         Different from `to_be_visible` as `to_be_visible` checks element has size > 1px
--- se_wrapper/element/expectations.py
+++ se_wrapper/element/expectations.py
@@ -18,7 +18,7 @@ class Expectations:
         self._web_element = web_element
         self._timeout = timeout
 
-    def to_be_visible(self, timeout: TimeoutType):
+    def be_visible(self, timeout: TimeoutType):
         """True when an element is visible on the html page.
         :param timeout: time to wait element visibility.
 
@@ -29,7 +29,7 @@ class Expectations:
         except TimeoutException:
             return False
 
-    def to_be_invisible(self, timeout: TimeoutType):
+    def be_invisible(self, timeout: TimeoutType):
         """True if an element is not visible on the page.
         :param timeout: time to wait element visibility.
 
@@ -40,7 +40,7 @@ class Expectations:
         except TimeoutException:
             return False
 
-    def to_have_class(self, expected_class: str, timeout: TimeoutType):
+    def have_class(self, expected_class: str, timeout: TimeoutType):
         """True when an element has a specific class.
         :param expected_class: class_name for expected class (not css_selector).
         :param timeout: time to wait for an element ho have a class name.
@@ -52,7 +52,7 @@ class Expectations:
         except TimeoutException:
             return False
 
-    def to_include_element(self, child_css_selector: str, timeout: TimeoutType):
+    def include_element(self, child_css_selector: str, timeout: TimeoutType):
         """True when an element gets a desired child element.
         :param child_css_selector: a css selector for a child element.
         :param timeout: time to wait for the condition.
@@ -68,7 +68,7 @@ class Expectations:
         except TimeoutException:
             return False
 
-    def to_contain_text(self, text: str, timeout: TimeoutType):
+    def contain_text(self, text: str, timeout: TimeoutType):
         """True if an element contains a provided text in its text attribute.
         :param text: expected text.
         :param timeout: time to wait for the condition.
@@ -82,7 +82,7 @@ class Expectations:
         except TimeoutException:
             return False
 
-    def to_have_similar_text(self, text: str, timeout: TimeoutType):
+    def have_similar_text(self, text: str, timeout: TimeoutType):
         """True if an element has a similar text in texts attribute.
         Not precise comparision, e.g. returns True for:
         "some" in "this is some text", " test\n" and "test", "TEST" and "test"
@@ -98,7 +98,7 @@ class Expectations:
         except TimeoutException:
             return False
 
-    def to_have_exact_text(self, text: str, timeout: TimeoutType):
+    def have_exact_text(self, text: str, timeout: TimeoutType):
         """True if an element has exactly provided text, and no other text.
         Precise comparision, e.g. returns False if "some" == "this is some text"
         :param text: exact text to search inside an element
@@ -127,7 +127,7 @@ class Expectations:
         except TimeoutException:
             return False
 
-    def to_be_on_the_screen(self, timeout: TimeoutType):
+    def be_on_the_screen(self, timeout: TimeoutType):
         """True for an element is present on the screen (inside the viewport).
         False if element's coordinates don't match viewport height and width.
         :param timeout: equal to the self.timeout if other not passed.
