ab348d2d81820ab4f8fbadb37bf7b4ab047c79dc
Viktor <vvgrygorchuk@gmail.com>
2019-11-17 20:30:19 +0200

    Added unit tests.

---
+++ tests/units_test/test_se_utils.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium.webdriver.common.by import By
+
+
+from se_wrapper.utils import se_utils
+
+
+@pytest.mark.parametrize("selector", ("div", ".class", "div[class='test']",
+                                      "img[class^='irc_mut'][src^='http']"))
+def test_css_selector(selector):
+    assert se_utils.get_selector_type(selector) == By.CSS_SELECTOR
+
+
+@pytest.mark.parametrize("selector", (
+    "//div", "./div/div/.class",
+    "//div[@id='rso']//div[@class='test tttest']//a[contains(@href, '/imgres')]",
+    "/html//div[contains(text(), 'The page')]",
+    "/bookstore/book[price>35.00]/title"
+))
+def test_xpath_selector(selector):
+    assert se_utils.get_selector_type(selector) == By.XPATH
