d04196f3111c522924aa82e2f5b2ba13afd9717e
Viktor <vvgrygorchuk@gmail.com>
2022-07-09 22:49:49 +0300

    Increase package version.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="selen-kaa",
-    version="0.1.0",
+    version="0.2.1",
     author="Viktor Grygorchuk",
     author_email="vvgrygorchuk@gmail.com",
     keywords=['Selenium', 'Test Automation'],
@@ -21,17 +21,17 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/VicGrygorchyk/selen_kaa.git",
-    download_url="https://github.com/VicGrygorchyk/selen_kaa/archive/0.1.0.tar.gz",
+    download_url="https://github.com/VicGrygorchyk/selen_kaa/archive/0.2.1.tar.gz",
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "test*"]),
     install_requires=get_requirements(),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Testing"
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
 )
